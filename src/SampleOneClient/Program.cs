5e2da56575f714e060dc626d872d4f635a774d25
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-09-04 09:27:09 +0800

    Adding the architecture of sample one

---
+++ src/SampleOneClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleOneClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
