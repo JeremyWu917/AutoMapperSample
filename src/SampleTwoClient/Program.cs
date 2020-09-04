bd84c1433119d9d52cd2e3550e349b029481cea3
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-09-04 16:06:44 +0800

    Add second sample about automapper

---
+++ src/SampleTwoClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleTwoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
